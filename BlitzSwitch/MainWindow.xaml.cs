be0e350fbff18029ab36c3ee671f2708f66609fb
Blitzmachine <blitz@BLITZOS>
2024-08-29 21:06:45 +0200

    Window Overall Done.

--- BlitzSwitch/MainWindow.xaml.cs
+++ BlitzSwitch/MainWindow.xaml.cs
@@ -5,7 +5,16 @@ namespace BlitzSwitch
 {
     public partial class MainWindow : Window
     {
-        public MainWindow() => InitializeComponent();
-        private void HeaderGrid_MouseDown(object sender, MouseButtonEventArgs e) => this.DragMove();
+        public MainWindow()
+            => this.InitializeComponent();
+
+        private void HeaderGrid_MouseDown(object sender, MouseButtonEventArgs e)
+            => this.DragMove();
+
+        private void MinimizeWindowButton_Click(object sender, RoutedEventArgs e)
+            => this.WindowState = WindowState.Minimized;
+
+        private void CloseWindowButton_Click(object sender, RoutedEventArgs e)
+            => Application.Current.Shutdown();
     }
 }
