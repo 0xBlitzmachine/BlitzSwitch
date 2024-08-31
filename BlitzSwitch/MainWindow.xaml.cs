d2ed9fb2f2e01ab23fac04059855f4c225a8a2c6
Blitzmachine <blitz@BLITZOS>
2024-08-31 21:49:38 +0200

    Visual setup done

--- BlitzSwitch/MainWindow.xaml.cs
+++ BlitzSwitch/MainWindow.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace BlitzSwitch
 {
     public partial class MainWindow : Window
     {
+
+        private bool isEnabled = false;
+
         public MainWindow()
             => this.InitializeComponent();
 
@@ -21,6 +25,56 @@ namespace BlitzSwitch
         {
             e.Handled = true;
             HotkeyTextBox.Text = e.Key.ToString();
+        }
+
+        private void SetHotkeyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AbortSettingHotkeyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HotkeyTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            SetHotkeyButton.IsEnabled = true;
+            AbortSettingHotkeyButton.IsEnabled = true;
+        }
+
+        private void HotkeyTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            SetHotkeyButton.IsEnabled = false;
+            AbortSettingHotkeyButton.IsEnabled = false;
+        }
+
+        private void SwitchStateButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.isEnabled = !this.isEnabled;
+            this.SwitchStateVisually(isEnabled);
+        }
+
+        private void SwitchStateVisually(bool isEnabled)
+        {
+            SolidColorBrush enabledColor = Application.Current.TryFindResource("EnabledColor") as SolidColorBrush;
+            SolidColorBrush disabledColor = Application.Current.TryFindResource("DisabledColor") as SolidColorBrush;
+
+            switch (isEnabled)
+            {
+                case true:
+                    this.BorderBrush = enabledColor;
+                    this.HeaderGrid.Background = enabledColor;
+                    this.HeaderStatusLabel.Text = "ENABLED";
+                    SwitchStateButton.Content = "DISABLE";
+                    break;
+                default:
+                    this.BorderBrush = disabledColor;
+                    this.HeaderGrid.Background = disabledColor;
+                    this.HeaderStatusLabel.Text = "DISABLED";
+                    SwitchStateButton.Content = "ENABLE";
+                    break;
+            }
         }
     }
 }
