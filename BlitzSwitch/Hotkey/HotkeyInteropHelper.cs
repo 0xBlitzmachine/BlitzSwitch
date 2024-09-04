498096d91df8a720e23125fd89f8815c7e25cd43
Blitzmachine <blitz@BLITZOS>
2024-09-04 16:50:31 +0200

    Preperation for global hotkey registration - IHotkeyManagerBase for the base needed requirements - HotkeyInteropHelper that imports the native RegisterHotkey and UnregisterHotkey win32 function - HotkeyManager holds the registered Key, the Handle of the Window, and to intercept window messages

---
+++ BlitzSwitch/Hotkey/HotkeyInteropHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BlitzSwitch.Hotkey
+{
+    internal static class HotkeyInteropHelper
+    {
+        // Import native functions from the win32 library
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+    }
+}
---
+++ BlitzSwitch/Hotkey/HotkeyManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace BlitzSwitch.Hotkey
+{
+    internal class HotkeyManager : IHotkeyManagerBase
+    {
+        public IntPtr HWnd { get; set; }
+        public Key Hotkey { get; set; }
+        public int HotkeyID { get; set; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RegisterHotkey(int id, int fsModifiers, int vk)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UnregisterHotkey(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
